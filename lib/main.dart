2946ed32e5200015f510d2f9b91432b78795047f
Josue Garcia <josgar_14@hotmail.com>
2022-04-22 18:57:50 -0400

    feat: my personal card

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,34 +14,80 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                height: double.infinity,
-                width: 100.0,
-                color: Colors.red,
+              const CircleAvatar(
+                radius: 100.0,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage('images/me.jpg'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.yellow,
+              const Text(
+                'Josue Garcia',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              const Text(
+                'SOFTWARE DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'Source',
+                    fontSize: 20.0,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5),
+              ),
+              SizedBox(
+                width: 150.0,
+                height: 20.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '999-999-999',
+                      style: TextStyle(
+                          color: Colors.teal[900],
+                          fontFamily: 'Source',
+                          fontSize: 20.0),
+                    ),
                   ),
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.green,
-                  )
-                ],
+                ),
               ),
-              Container(
-                width: 100.0,
-                height: double.infinity,
-                color: Colors.blue,
-              )
+              Card(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'myUser@hotmail.com',
+                      style: TextStyle(
+                          color: Colors.teal[900],
+                          fontFamily: 'Source',
+                          fontSize: 20.0),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
@@ -49,7 +95,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-/*
-
- */
